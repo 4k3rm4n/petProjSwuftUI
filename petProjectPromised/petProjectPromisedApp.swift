07f69e0f9c35cd3a88fa2678d2996907cd2cb411
Peretiatko Bohdan <bohdan_peretiatko@epam.com>
2025-09-25 18:09:45 +0300

    Initial Commit

---
+++ petProjectPromised/petProjectPromisedApp.swift
@@ -0,0 +1,17 @@
+//
+//  petProjectPromisedApp.swift
+//  petProjectPromised
+//
+//  Created by Bohdan Peretiatko on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct petProjectPromisedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
